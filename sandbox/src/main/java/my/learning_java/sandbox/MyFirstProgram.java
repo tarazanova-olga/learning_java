74b9017f61c0364c4a3c28ea114f65af218a5858
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-09-17 15:46:29 +0400

    Добавление нескольких функций

--- sandbox/src/main/java/my/learning_java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/my/learning_java/sandbox/MyFirstProgram.java
@@ -4,13 +4,29 @@ public class MyFirstProgram
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, world!");
-		System.out.println(2+2);
-		System.out.println(2-1);
-		System.out.println(2*4);
-		System.out.println(2/1);
-		System.out.println(2.0/4.5);
-		System.out.println("Сложение"+" "+"строк");
-		System.out.println("2 + 2 = " + (2+2));
+		double len = 5;
+		double a = 5;
+		double b = 12;
+		hello("world");
+		hello("olga");
+		System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+		System.out.println("Площадь прямоугольника со сторонами " + a  + " и " + b + " = " + area(a,b));
+		
+	}
+	public static void hello(String name)
+	{
+		System.out.println("Hello, " + name + "!");
+	}
+	
+	//функция для расчета площади квадрата, принимающая 1 параметр
+	public static double area(double l)
+	{
+		return l*l;
+	}
+		
+	//функция для расчета площади прямоугольника, принимающая 2 параметра
+	public static double area(double a, double b)
+	{
+		return a*b;
 	}
 }
