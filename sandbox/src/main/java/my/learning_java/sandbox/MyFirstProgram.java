e86684c5caeab526dbb925edc06b68facf72d261
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-09-17 14:16:30 +0400

    Эксперименты с операциями над числами и строками

--- sandbox/src/main/java/my/learning_java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/my/learning_java/sandbox/MyFirstProgram.java
@@ -5,5 +5,12 @@ public class MyFirstProgram
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, world!");
+		System.out.println(2+2);
+		System.out.println(2-1);
+		System.out.println(2*4);
+		System.out.println(2/1);
+		System.out.println(2.0/4.5);
+		System.out.println("Сложение"+" "+"строк");
+		System.out.println("2 + 2 = " + (2+2));
 	}
 }
