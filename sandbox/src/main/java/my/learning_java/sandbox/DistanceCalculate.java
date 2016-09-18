3be6c9c66d2a356d2bb574bfc1b6714679a12370
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-09-18 17:39:39 +0400

    Исправления по расчету расстояния между двумя точками

--- sandbox/src/main/java/my/learning_java/sandbox/DistanceCalculate.java
+++ sandbox/src/main/java/my/learning_java/sandbox/DistanceCalculate.java
@@ -5,9 +5,20 @@ public class DistanceCalculate
 	
 	public static void main(String[] args)
 	{
-		Point p = new Point(5,2,6,4);
-		System.out.println("Расстояие между точкой с координатами " + p.x1 + " и " + p.y1
-		+ " и точкой с координатами " + p.x2 + " и " + p.y2 + " = " + p.distance());
+		Point p1 = new Point(5,6);
+		Point p2 = new Point(6,4);
+		
+		System.out.println("Расстояие между точкой с координатами "+ p1.x + " и " + p1.y
+		+ " и точкой с координатами " + p2.x + " и " + p2.y + " = " + p1.distancePoints(p2) + " расчет через метод" );
+		
+		System.out.println("Расстояие между точкой с координатами "+ p1.x + " и " + p1.y
+		+ " и точкой с координатами " + p2.x + " и " + p2.y + " = " + distance(p1, p2) + " расчет через функцию" );
 	}
 	
+	public static double distance(Point p1, Point p2)
+  {
+		return Math.sqrt(((p2.x-p1.x)*(p2.x-p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
+						
+  }
+
 }
