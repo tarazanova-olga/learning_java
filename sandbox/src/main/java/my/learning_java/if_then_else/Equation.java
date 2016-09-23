c1093d2ea9c959a91a851d679ee7526126135fb1
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-09-23 16:54:34 +0300

    Тесты на проверку корней квадратного уравнения

--- sandbox/src/main/java/my/learning_java/if_then_else/Equation.java
+++ sandbox/src/main/java/my/learning_java/if_then_else/Equation.java
@@ -18,13 +18,12 @@ public class Equation {
 
         if (d>0){
             n=2;
-        } else {
-            if (d == 0){
-                n = 1;
+        } else if (d == 0){
+            n = 1;
             } else {
-                n = 0;
+            n = 0;
             }
-        }
+
     }
 
     public int rootNumber(){
