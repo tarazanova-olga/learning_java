eb6bb2aca3363af95037b0076619f3156d40e41c
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-09-26 12:21:54 +0300

    Добавление метода для перехода на домашнюю страницу, с проверкой необходимости этого перехода

--- addressbook_web_tests/src/test/java/my/learning_java/addressbook/tests/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/my/learning_java/addressbook/tests/ContactCreationTest.java
@@ -7,7 +7,7 @@ public class ContactCreationTest extends TestBase {
 
     @Test
     public void testContactCreationTest() {
-
+        app.getNavigationHelper().gotoHomePage();
         app.getContactHelper().initContactCreation();
         app.getContactHelper().fillContactPage(new ContactData("Иван", "Иванович", "Иванов", "89651231123",
                 "test@test.test", "ул. Мира, д.123", "Новая группа"), true);
