b1ceb1719f937d5a5394ec7eff2ad411958daed0
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-10-20 11:12:42 +0300

    Параметризация тестовых методов в тесте создания контакта

--- addressbook_web_tests/src/test/java/my/learning_java/addressbook/tests/tests_for_contacts/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/my/learning_java/addressbook/tests/tests_for_contacts/ContactCreationTest.java
@@ -3,22 +3,36 @@ package my.learning_java.addressbook.tests.tests_for_contacts;
 import my.learning_java.addressbook.model.ContactData;
 import my.learning_java.addressbook.model.Contacts;
 import my.learning_java.addressbook.tests.TestBase;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactCreationTest extends TestBase {
+public class ContactCreationTest extends TestBase{
 
-    @Test
-    public void testContactCreationTest() {
+    @DataProvider
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        File photo = new File("src/test/resources/ленин.jpg");
+        list.add(new Object[] {new ContactData().withName("Иван").withMiddleName("Иванович").withLastName("Иванов").withHomePhone("89651231123")
+                .withEmail("test@test.test").withAddress("ул. Мира, д.123").withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withName("Иван").withMiddleName("Иванович").withLastName("Иванов").withHomePhone("89651231123")
+                .withEmail("test@test.test").withAddress("ул. Мира, д.123").withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withName("Иван").withMiddleName("Иванович").withLastName("Иванов").withHomePhone("89651231123")
+                .withEmail("test@test.test").withAddress("ул. Мира, д.123").withPhoto(photo)});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreationTest(ContactData contact) {
         app.goTo().HomePage();
         Contacts before =  app.contact().all();
-        File photo = new File("src/test/resources/ленин.jpg");
-        ContactData contact = new ContactData().withName("Иван").withMiddleName("Иванович").withLastName("Иванов").withHomePhone("89651231123")
-                .withEmail("test@test.test").withAddress("ул. Мира, д.123").withPhoto(photo);
         app.contact().createContact(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after =  app.contact().all();
