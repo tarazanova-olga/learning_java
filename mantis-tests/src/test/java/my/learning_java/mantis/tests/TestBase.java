21a78701a04ca9e0e7d94f3d5b5e80d9fd2b7724
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-10-31 17:13:04 +0300

    Добавление нового модуля для тестов Mantis

---
+++ mantis-tests/src/test/java/my/learning_java/mantis/tests/TestBase.java
@@ -0,0 +1,24 @@
+package my.learning_java.mantis.tests;
+
+
+import my.learning_java.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun=true)
+    public void tearDown() {
+
+        app.stop();
+    }
+}
