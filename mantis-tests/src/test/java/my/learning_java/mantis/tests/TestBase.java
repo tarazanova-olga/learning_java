319a98cffbedb8204ffde287ee4538545301ead5
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-11-02 13:42:30 +0300

    Правки по ftp-подключению

--- mantis-tests/src/test/java/my/learning_java/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/my/learning_java/mantis/tests/TestBase.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestBase {
 
     protected static final ApplicationManager app
@@ -14,11 +17,12 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.back");
     }
 
     @AfterSuite(alwaysRun=true)
-    public void tearDown() {
-
+    public void tearDown() throws IOException {
+        app.ftp().restore("config_inc.php.back", "config_inc.php");
         app.stop();
     }
 }
