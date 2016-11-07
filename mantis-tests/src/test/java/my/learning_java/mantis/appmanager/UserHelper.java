ba62286c0b36580d687eba6e0f5fd5a7cdebfadb
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-11-07 13:10:04 +0300

    Правки по тесту на сброс пароля у юзера

--- mantis-tests/src/test/java/my/learning_java/mantis/appmanager/UserHelper.java
+++ mantis-tests/src/test/java/my/learning_java/mantis/appmanager/UserHelper.java
@@ -1,6 +1,7 @@
 package my.learning_java.mantis.appmanager;
 
 
+import org.openqa.selenium.By;
 
 public class UserHelper extends BaseHelper{
 
@@ -10,6 +11,21 @@ public class UserHelper extends BaseHelper{
     }
 
     public void goToUserPage(){
-        wd.get(app.getProperty("web.baseURL") + "/manage_user_page.php");
+        wd.get(app.getProperty("web.baseURL") + "/manage_user_edit_page.php?user_id=2");
     }
+
+    public String parseNameSelectedUser(){
+       String name = wd.findElement(By.xpath("//input[@name='username']")).getAttribute("value");
+        return name;
+    }
+
+    public String parseEmailSelectedUser(){
+        String email = wd.findElement(By.xpath("//input[@name='email']")).getAttribute("value");
+        return email;
+    }
+
+    public void resetPassword(){
+        click(By.xpath("//input[@value='Сбросить пароль']"));
+    }
+
 }
--- mantis-tests/src/test/java/my/learning_java/mantis/tests/ResetPasswordTests.java
+++ mantis-tests/src/test/java/my/learning_java/mantis/tests/ResetPasswordTests.java
@@ -1,9 +1,17 @@
 package my.learning_java.mantis.tests;
 
 
+import my.learning_java.mantis.model.MailMessage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 
 public class ResetPasswordTests extends TestBase {
     @BeforeMethod
@@ -12,17 +20,27 @@ public class ResetPasswordTests extends TestBase {
     }
 
     @Test
-    public  void testResetPassword(){
+    public  void testResetPassword() throws IOException, MessagingException {
         app.login().start("administrator", "root");
         app.user().goToUserPage();
-//        String users = app.user().all();
-
+        String name = app.user().parseNameSelectedUser();
+        String email = app.user().parseEmailSelectedUser();
+        String password = "newpassword";
+        app.user().resetPassword();
+        List<MailMessage> mailMessages = app.mail().waitForMail(1, 1000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password);
+        assertTrue(app.newSession().login(name, password));
     }
 
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMassage = mailMessages.stream().filter((m)->m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMassage.text);
+    }
 
     @AfterMethod(alwaysRun = true)
     public void stopMailServer(){
-
         app.mail().stop();
     }
 
