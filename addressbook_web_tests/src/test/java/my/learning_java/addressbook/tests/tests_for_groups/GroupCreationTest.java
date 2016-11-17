513960508a2b42be71d1691d247644083c12445a
tarazanova-olga <olga.v.tarazanova@gmail.com>
2016-11-17 17:08:13 +0300

    Изменения для проверки генерации отчетов через allure

--- addressbook_web_tests/src/test/java/my/learning_java/addressbook/tests/tests_for_groups/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/my/learning_java/addressbook/tests/tests_for_groups/GroupCreationTest.java
@@ -77,7 +77,7 @@ public class GroupCreationTest extends TestBase {
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
-        assertThat(after.size(), equalTo(before.size() - 1));
+        assertThat(after.size(), equalTo(before.size() + 1));
 
         assertThat(after, equalTo
                 (before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getGroupId()).max().getAsInt()))));
